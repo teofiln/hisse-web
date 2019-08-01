a629f9146d553fd2ea26130b67afe35639c77f46
teofiln <teofiln@gmail.com>
2019-08-01 05:59:39 -0500

    References box

--- app.R
+++ app.R
@@ -6,6 +6,23 @@ library(utilhisse)
 library(viridis)
 library(colorplaner)
 
+refs_text <- tags$div(
+  tags$p("This web application is based on the following papers and R packages:"),
+  tags$p("Papers:"),
+  tags$p(
+      "Beaulieu, J. M., & O’Meara, B. C. (2016). Detecting hidden diversification shifts in models of trait-dependent speciation and extinction. Systematic biology, 65(4), 583-601.", tags$a(href="https://academic.oup.com/sysbio/article/65/4/583/1753616", "link")
+    ),
+  tags$p(
+      "Caetano, D. S., O'Meara, B. C., & Beaulieu, J. M. (2018). Hidden state models improve state‐dependent diversification approaches, including biogeographical models. Evolution, 72(11), 2308-2324.", tags$a(href="https://onlinelibrary.wiley.com/doi/abs/10.1111/evo.13602", "link")
+    ),
+  tags$p(
+      "Nakov, T., Beaulieu, J. M., & Alverson, A. J. (2019). Diatoms diversify and turn over faster in freshwater than marine environments. bioRxiv, 406165.", tags$a(href="https://doi.org/10.1101/406165", "link")
+    ),
+  tags$p("R packages:"),
+  tags$p("Beaulieu, J., O'Meara, B., & Caetano, D. (2019). Package ‘hisse’", tags$a(href="https://cran.r-project.org/web/packages/hisse/index.html", 'CRAN')),
+  tags$p("Nakov, T. (2019). Package ’utilhisse’", tags$a(href="https://github.com/teofiln/utilhisse", 'Guthub'))
+  )
+
 # modules
 mods <-
   list.files(path = "modules/",
@@ -35,7 +52,19 @@ ui <-
                helpText(
                  'This should be an object output from `hisse::MarginRecon` or a list of such objects where each element contains the AIC score of the model. Make sure the object was saved to a file with the extension "Rsave" or "Rdata".'
                ),
-               checkboxInput("h_demo", label = "Use demo file", value = FALSE)
+               checkboxInput("h_demo", label = "Use demo file", value = FALSE),
+               conditionalPanel(condition = "input.h_demo", 
+                                HTML("This is a model fit for a CID4 model for plankton-benthos trait in diatoms. For more information see our paper: <a href=https://doi.org/10.1101/406165>'Biorxiv'</a>.")),
+               tags$hr(),
+               checkboxGroupButtons(
+                 inputId = "references",
+                 choiceNames = "References",
+                 choiceValues = 1,
+                 status = "success",
+                 selected = "References"
+               ),
+               conditionalPanel(condition = "input.references == 1",
+                                wellPanel(refs_text))
              )),
       column(
         9,
@@ -61,8 +90,20 @@ ui <-
                helpText(
                  'This should be an object output from `hisse::MarginReconMuHiSSE` or a list of such objects where each element contains the AIC score of the model. Make sure the object was saved to a file with the extension "Rsave" or "Rdata".'
                ),
-               checkboxInput("m_demo", label = "Use demo file", value = FALSE)
-             )),
+               checkboxInput("m_demo", label = "Use demo file", value = FALSE),
+               conditionalPanel(condition = "input.m_demo", 
+                                HTML("This is a model fit for a MuHiSSE model for marine-freshwater + plankton-benthos interaction in diatoms. For more information see our paper: <a href=https://doi.org/10.1101/406165>'Biorxiv'</a>.")),
+      tags$hr(),
+      checkboxGroupButtons(
+        inputId = "references2",
+        choiceNames = "References",
+        choiceValues = 1,
+        status = "success",
+        selected = "References"
+      ),
+      conditionalPanel(condition = "input.references2 == 1",
+                       wellPanel(refs_text))
+    )),
       column(
         9,
         m_scatterplot_ui(id = "11"),
